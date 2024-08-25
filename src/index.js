42a55b27ea0e3a1d3fb7c8552b05290b30d518c6
bkvats <bkvats2394@gmail.com>
2024-08-26 00:14:51 +0530

    middleware setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import 'dotenv/config';
 import connectDB from "./db/index.js";
-connectDB();
+import app from './app.js';
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("DATABASE CONNECTION FAILED:", error);
+});
