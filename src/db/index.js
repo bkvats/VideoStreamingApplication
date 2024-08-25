bb64ef59307b3aa726bd3e36a76eec5964966f34
bkvats <bkvats2394@gmail.com>
2024-08-25 23:39:18 +0530

    database setup

---
+++ src/db/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+export default async function connectDB() {
+    try {
+        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("CONNECTED SUCCESFULLY, DB HOST:", connection.connection.host);
+    } catch (error) {
+        console.log("DATABASE CONNECTION FAILED:", error);
+        process.exit(1);
+    }
+}
