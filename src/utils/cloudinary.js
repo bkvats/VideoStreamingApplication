f125f928c369094d36e54615cb3e516348e0295e
bkvats <bkvats2394@gmail.com>
2024-08-29 00:09:40 +0530

    adding multer middleware and cloudinary utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,17 @@
+import {v2 as cloudniary} from "cloudinary";
+import fs from "fs";
+cloudniary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+export default async function uploadOnCloudinary(localFilePath) {
+    try {
+        if (localFilePath) {
+            const result = await cloudniary.uploader.upload(localFilePath, {resource_type: "auto"});
+            console.log("FILE SUCCESFULLY UPLOADED ON CLOUDINARY AT:", result.url);
+        }
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+    }
+}
