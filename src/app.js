5dbb1f842c261c9752fe57a635e8b5ed1f5fcdf3
bkvats <bkvats2394@gmail.com>
2024-09-01 23:15:54 +0530

    creating apis for data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -10,4 +11,5 @@ app.use(express.json({limit: "32kb"}));
 app.use(express.urlencoded({limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use("/api/v1/users", userRouter)
 export default app;
